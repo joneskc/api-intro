a2446232313bc53bb93cd36c4a083f0d33d114cc
Keith Jones <keithjones@Keiths-MacBook-Pro.local>
2018-03-20 18:52:27 -0600

    continued with exercise

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 ## Objectives
 
 - Explain what an API is
+    - Application Programming Interface. It's a way for us to interact with other pieces of software
+    - internal API: things that are native to a language. fetch itself: asynchronous requests.
+    - AJAX asynchronous javascript and XML
+    - external API: usually a server and a database that exists on someone else's machine. we can request data from the server, server queries the database and then the server packages up data and sends it to us
 - Use fetch to GET data from an external API
 - Append data from a fetch request to the DOM
 
--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const baseUrl = 'https://swapi.co/api/'
 const characterSection = document.querySelector('.characters')
 
+// explain what an API is (internal and external)
+// use fetch to get data from an external API
+// once we have data back from fetch request, how to append it to the DOM
+// stretch goal: identify API's you'd like to use
+
 // Write a function that GETs information about Luke Skywalker and add his name to the page
 getLuke()
 
@@ -11,4 +16,79 @@ getLuke()
 
 function getLuke() {
 
+  fetch(baseUrl + 'people/1')
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(data) {
+      console.log(data);
+
+      var name = data.name
+      var skywalker = document.createElement('h2')
+      skywalker.innerText = name
+      characterSection.prepend(skywalker)
+      var hairColor = data.hair_color
+      var lukeHair = document.createElement('li')
+      var height = data.height
+      var lukeHeight = document.createElement('li')
+      lukeHair.innerText = hairColor
+      lukeHeight.innerText = height
+      document.querySelector('.attributes').appendChild(lukeHair)
+      document.querySelector('.attributes').appendChild(lukeHeight)
+    })
+
 }
+
+
+getCharacters()
+
+function getCharacters() {
+
+  fetch(baseUrl + 'people')
+    .then(function(people) {
+      return people.json()
+    })
+    .then (function(allPeople) {
+      console.log(allPeople)
+      for (i=0; i<allPeople.results.length; i++) {
+        var characterArr = allPeople.results[i].name
+        console.log(characterArr)
+
+      }
+      }
+    })
+}
+
+
+// Modify the getLuke function to also add:
+  // Luke's hair color
+  // Luke's height
+
+// to the ul with the class of 'attributes'
+
+
+// Uh oh, Luke's attributes are ABOVE his name!?
+  // Explain why this happened
+  // Modify the getLuke function to ensure Luke's attributes are below his name
+
+
+// Write a function called getCharacters that:
+  // Makes a fetch request to the baseUrl + '/people/ endpoint
+  // console.log the resulting response
+
+
+// Modify the getCharacters function to loop through the results
+  // console.log each character in the array
+    // (see what you're working with)
+  // Capture each character's attributes in variables:
+    // name
+    // hair color
+    // height
+  // console.log each variable
+
+
+// Modify the getCharacters function to append a section for each character
+  // Container element (<div> or <section>)
+  // <h3> with the character's name
+  // <ul> with  <li>s for hair color and height
+  // check out the innerHTML method
